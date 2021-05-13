4ef4c826fe863a5babc07532adaa62d873a6189e
osydorchenko <83658635+osydorchenko@users.noreply.github.com>
2021-05-13 21:39:45 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -5,6 +5,6 @@ link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 def test_guest_should_see_basket_button(browser):
     browser.get(link)
-    time.sleep(5)
+    time.sleep(30)
     button = browser.find_elements_by_css_selector('button.btn-add-to-basket')
     assert len(button) == 1, 'No such button on a page'
